46062aaca47376b2b34e1c24caf9ad3a207665ea
Elmira <e.nasirova@pnlfin.tech>
2021-06-17 16:49:22 +0300

    add local execute and screenshot if test failed

--- conftest.py
+++ conftest.py
@@ -1,26 +1,34 @@
-import pytest
+import logging
 import os
 
+import allure
+import pytest
 from selenium import webdriver
+
+logging.basicConfig(level=logging.INFO, filename=os.getcwd() + "/selenium.log")
 
 
 def pytest_addoption(parser):
-    parser.addoption("--browser", action="store", default='chrome')
-    parser.addoption("--browversion", action="store", default="88.0")
-    parser.addoption("--executor", action="store", default="192.168.0.102")
+    parser.addoption("--browser", action="store", default='firefox')
+    parser.addoption("--browversion", action="store", default="89.0")
+    parser.addoption("--executor", action="store", default="192.168.0.100")
+    parser.addoption("--localexecute", action="store", default=True)
+    parser.addoption("--headless", action="store_true", help="Run headless")
 
 
 @pytest.fixture
 def browser(request):
     driver = None
 
+    local = request.config.getoption("--localexecute")
     browser = request.config.getoption("--browser")
     version = request.config.getoption("--browversion")
     executor = request.config.getoption("--executor")
+    headless = request.config.getoption("--headless")
 
     add_environments_properties(browser, version)
 
-    if browser.lower() != 'safari':
+    if not local and browser.lower() != 'safari':
         executor_url = f"http://{executor}:4444/wd/hub"
 
         capabilities = {
@@ -38,12 +46,32 @@ def browser(request):
             command_executor=executor_url,
             desired_capabilities=capabilities)
 
+    elif local and browser == "chrome":
+        options = webdriver.ChromeOptions()
+        if headless: options.headless = True
+        driver = webdriver.Chrome(
+            executable_path='//Users//finomteam//Desktop//Drivers//chromedriver',
+            options=options)
+
+    elif local and browser == "firefox":
+        options = webdriver.FirefoxOptions()
+        if headless: options.headless = True
+        driver = webdriver.Firefox(
+            executable_path='//Users//finomteam//Desktop//Drivers//geckodriver',
+            options=options)
+
     elif browser == "safari":
         driver = webdriver.Safari()
 
     driver.maximize_window()
 
     def teardown():
+        if request.node.rep_call.failed:
+            allure.attach(
+                body=driver.get_screenshot_as_png(),
+                name=request.function.__name__,
+                attachment_type=allure.attachment_type.PNG)
+
         driver.quit()
 
     request.addfinalizer(teardown)
@@ -53,8 +81,16 @@ def browser(request):
 
 def add_environments_properties(browser, version):
     with open('environment.properties', 'w') as f:
-        f.write("Browser={browser}\n".format(browser = browser))
-        f.write("Browser.Version={version}".format(version = version))
+        f.write("Browser={browser}\n".format(browser=browser))
+        f.write("Browser.Version={version}".format(version=version))
 
     folder_path = os.getcwd()
     os.replace("environment.properties", folder_path + "/allure-results/environment.properties")
+
+
+@pytest.hookimpl(hookwrapper=True, tryfirst=True)
+def pytest_runtest_makereport(item):
+    outcome = yield
+    rep = outcome.get_result()
+    setattr(item, "rep_" + rep.when, rep)
+    return rep
